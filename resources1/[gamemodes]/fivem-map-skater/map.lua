72dd39209f9b0a47d63435f79646ccd886ec8e9c
320thSTSMilSim <ryan.king3481@gmail.com>
2018-09-22 11:52:56 -0700

    Add files via upload

---
+++ resources1/[gamemodes]/fivem-map-skater/map.lua
@@ -0,0 +1,107 @@
+spawnpoint 'a_f_m_downtown_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_clown_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_m_eastsa_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_construct_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_m_eastsa_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_devinsec_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_m_fatbla_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_dockwork_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_m_ktown_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_dwservice_02' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_m_prolhost_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_grip_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_m_skidrow_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_marine_02' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_m_soucent_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_pestcont_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_m_soucent_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_shop_mask' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_o_genstreet_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_xmech_02_mp' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_o_indian_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_f_m_promourn_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_o_salton_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_f_y_poppymich' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_o_soucent_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_m_promourn_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_o_soucent_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_m_prolsec_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_bevhills_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_abner' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_bevhills_03' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_antonb' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_eastsa_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_baygor' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_bevhills_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_burgerdrug_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_bevhills_04' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_chip' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_business_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_cyclist_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_business_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_fibmugger_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_business_03' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_guido_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_business_04' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_gunvend_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_eastsa_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_mani' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_eastsa_03' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_militarybum' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_epsilon_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_militarybum' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_fitness_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_paparazzi' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_genhot_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_party_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_tourist_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_pogo_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_vinewood_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_pogo_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_vinewood_02' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_pogo_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_vinewood_03' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_pogo_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_vinewood_04' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_proldriver_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_f_y_yoga_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_sbike' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_bevhills_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_staggrm_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_eastsa_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_staggrm_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_hillbilly_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 'u_m_y_tattoo_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_malibu_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_linecook' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_mexcntry_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_lsmetro_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_og_boss_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_mariachi_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_paparazzi_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_migrant_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_polynesian_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_movalien_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_rurmeth_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_movprem_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_salton_04' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_movspace_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_skidrow_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_paramedic_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_socenlat_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_m_postal_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+spawnpoint 'a_m_m_tennis_01' { x = 1856.21, y =  3681.44, z = 34.2675 }               
+spawnpoint 's_m_y_busboy_01' { x = -442.147, y =  6017.7, z = 31.6667 } 
+
+--
+
+
+
+
+
+
+
+
+#Edited by Vasco
+
+
