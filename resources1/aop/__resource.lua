b3729e87f46b4e6cda877b8d021bcda7901fb6b2
320thSTSMilSim <ryan.king3481@gmail.com>
2018-09-22 11:58:40 -0700

    Add files via upload

---
+++ resources1/aop/__resource.lua
@@ -0,0 +1,15 @@
+----------------------------------
+-- Area of Patrol, Made by FAXES--
+----------------------------------
+--
+-- Use the Config found in the server.lua to enable / disable the peacetime and vote commands.
+--
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_script "client.lua"
+server_script "server.lua"
+
+--[[
+    It's recommended to read the Licence for this resource
+        http://faxes.zone/scripts/terms
+]]
---
+++ resources1/aop/server.lua
@@ -0,0 +1,124 @@
+----------------------------------
+-- Area of Patrol, Made by FAXES--
+----------------------------------
+
+--- CONFIG AREA ---
+vote = false				-- Enables the vote command for AOP
+peacetime = false		-- Enables the peacetime commands for AOP
+
+--- CODE SPACE DON'T TOUCH ---
+
+FaxCurAOP = "Sandy"
+FaxCurAOP2 = "Shores"
+Faxvotestatus = false
+
+AddEventHandler('onResourceStart', function(resource)
+	Citizen.Wait(15000)
+	SetMapName("RP : Not Set")
+	CancelEvent()
+end)
+
+AddEventHandler('chatMessage', function(source, name, msg)
+	local args = stringsplit(msg, " ")
+	if args[1] == "/aop" then
+		if IsPlayerAceAllowed(source, "faxes.aopcmds") then
+			if tablelength(args) > 2 then
+			CancelEvent()
+			FaxCurAOP = args[2]
+			FaxCurAOP2 = args[3]
+			TriggerEvent('AOP:Sync')
+			TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n RP AREA IS NOW : " .. FaxCurAOP .. " " .. FaxCurAOP2 .. " \n Please Finish Your Current RP and Move. \n ——————————————————————", {239, 0, 0})
+			SetMapName("RP : " .. FaxCurAOP .. " " .. FaxCurAOP2)
+			else
+				TriggerClientEvent("chatMessage", source, "Invalid Input, Use 2 Words (Eg; Sandy Shores)")
+			end
+		else
+			TriggerClientEvent('AOP:NoPerms', source)
+		end
+	end
+end)
+
+
+RegisterServerEvent('AOP:Sync')
+AddEventHandler('AOP:Sync', function()
+	TriggerClientEvent('AOP:SendAOP', -1, FaxCurAOP, FaxCurAOP2)
+end)
+
+
+AddEventHandler('chatMessage', function(source, name, msg)
+	-- AOP Command for Peace Time on
+	local args = stringsplit(msg, " ")
+	if args[1] == "/pton" then
+		if peacetime == true then
+			if IsPlayerAceAllowed(source, "faxes.aopcmds") then
+				CancelEvent()
+				TriggerClientEvent('AOP:SendPeaceTrue', -1)
+				TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n PEACE TIME IS NOW IN EFFECT \n This Means No Priority Calls.  \n ——————————————————————", {239, 0, 0})
+				SetGameType("Peace Time In Effect")
+			else
+				TriggerClientEvent('AOP:NoPerms', source)
+			end
+		end
+  	end
+	-- AOP Command for Peace Time off
+	local args = stringsplit(msg, " ")
+	if args[1] == "/ptoff" then
+		if peacetime == true then
+			if IsPlayerAceAllowed(source, "faxes.aopcmds") then
+				CancelEvent()
+				TriggerClientEvent('AOP:SendPeaceFalse', -1)
+				TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n PEACE TIME IS NO LONGER IN EFFECT \n Resume Normal RP.  \n ——————————————————————", {239, 0, 0})
+				SetGameType("Freeroam")
+			else
+				TriggerClientEvent('AOP:NoPerms', source)
+			end
+		end
+  	end
+	-- AOP Command for Vote
+	RegisterCommand("aopvote", function()
+		if vote == true then
+			if IsPlayerAceAllowed(source, "faxes.aopcmds") then
+				Faxvotestatus = true
+				TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n RP AREA VOTE \n Use /vote. Vote Lasts 3 Minutes.  \n ——————————————————————", {239, 0, 0})
+				Wait(120000)
+				TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n RP AREA VOTE \n Use /vote. 1 Minute Remaining.  \n ——————————————————————", {239, 0, 0})
+				Wait(60000)
+				TriggerClientEvent("chatMessage", -1, " \n —————————————————————— \n RP AREA VOTE \n Has Ended. Please Wait For Change.  \n ——————————————————————", {239, 0, 0})
+				Faxvotestatus = false
+			else
+				TriggerClientEvent('AOP:NoPerms', source)
+			end
+		end
+	end)
+end)
+
+AddEventHandler('chatMessage', function(source, name, msg)
+	-- Vote Types
+	sm = stringsplit(msg, " ");
+	if sm[1] == "/vote" then
+		if Faxvotestatus == true then
+		CancelEvent()
+		TriggerClientEvent('chatMessage', -1, "^5" .. name .. " ^4Has Voted For: ^7" .. string.sub(msg,6))
+		else
+			TriggerClientEvent('AOP:NoVote', source)
+		end
+	end
+end)
+
+function stringsplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
